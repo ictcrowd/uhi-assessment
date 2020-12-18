7a582ecdea51ac8759a692f70b0d3ac63aad7fea
ictcrowd <76042522+ictcrowd@users.noreply.github.com>
2020-12-18 13:15:06 +0000

    Update mathscript.js

--- mathscript.js
+++ mathscript.js
@@ -1,34 +1,17 @@
-<script>
-		   var minimum = 1;
-		   var maximum = 100;
-		   
-		   var int1 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-		   var int2 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-		   
-		   document.getElementById('question').innerHTML = int1 + " " + "+" + " " + int2;
-		   var qanswer = int1 + int2;
-			
-
-			var minimum = 1;
-			var maximum = 100;
-			
-			var int1 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-			var int2 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-			
-			document.getElementById('question').innerHTML = int1 ;
-			
-			var qanswer = int1 + int2;
-			
-			function fire() {
-			  
-			  var uanswer = document.getElementById('answer').value;
-			  if (uanswer == qanswer) {
-			    alert("correct");
-			    
-			  } else {
-			    
-			    alert("incorrect")
-			  }
-			  
-			}
-			</script>
+var minimum = 1;
+var maximum = 100;
+var int1 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+var int2 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+document.getElementById('question').innerHTML = int1 + " " + "+" + " " + int2;
+var qanswer = int1 + int2;
+ 
+    function fire() {
+var uanswer = document.getElementById('answer').value;
+      if (uanswer == qanswer) {
+        alert("Nice math skills! Refresh the page to play again!");
+        
+        
+      } else {
+        alert("WRONG! Don't snooze during math class!")
+      }
+    }
