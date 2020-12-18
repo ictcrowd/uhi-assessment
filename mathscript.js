67d2d86b5dac024aceb5e31a42eefe19743bddc0
ictcrowd <76042522+ictcrowd@users.noreply.github.com>
2020-12-18 13:09:02 +0000

    Add files via upload

---
+++ mathscript.js
@@ -0,0 +1,34 @@
+<script>
+		   var minimum = 1;
+		   var maximum = 100;
+		   
+		   var int1 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+		   var int2 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+		   
+		   document.getElementById('question').innerHTML = int1 + " " + "+" + " " + int2;
+		   var qanswer = int1 + int2;
+			
+
+			var minimum = 1;
+			var maximum = 100;
+			
+			var int1 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+			var int2 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+			
+			document.getElementById('question').innerHTML = int1 ;
+			
+			var qanswer = int1 + int2;
+			
+			function fire() {
+			  
+			  var uanswer = document.getElementById('answer').value;
+			  if (uanswer == qanswer) {
+			    alert("correct");
+			    
+			  } else {
+			    
+			    alert("incorrect")
+			  }
+			  
+			}
+			</script>
